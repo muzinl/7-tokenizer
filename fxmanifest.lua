3b1c8f0c6375a320d0252b42f81e42a96ecaf9a2
Muzi. <114067445+muzinl@users.noreply.github.com>
2025-03-31 21:14:50 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+
+name '7-tokenizer'
+version '1.0.0'
+description 'Secure token system'
+
+server_scripts {
+    'server.lua'
+}
+
+server_exports {
+    'checkToken'
+}
